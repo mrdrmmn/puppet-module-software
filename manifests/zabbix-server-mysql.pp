dfe6b17a3a328bfad1995309e33aa80608f8c829
Michael Johnson - MJ <mj@ve01.revmj.com>
2011-01-31 03:44:05 +0000

    Added zabbix-server-mysql

---
+++ manifests/zabbix-server-mysql.pp
@@ -0,0 +1,15 @@
+# Class: software::slapd
+#
+# This is the git class in the software module.
+#   
+# Parameters:
+#   
+# Actions:
+#   
+# Requires:
+#   
+# Sample Usage:
+#   
+# [Remember: No empty lines between comments and class definition]
+class software::zabbix-server-mysql {
+}
