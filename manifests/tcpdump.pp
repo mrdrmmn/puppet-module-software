8cb6b7b3a520f07ba6d17ab06fb65338f35b77c3
Michael Johnson - MJ <mj@revmj.com>
2011-01-31 17:29:05 -0800

    Added tcpdump

--- manifests/tcpdump.pp
+++ manifests/tcpdump.pp
@@ -1,7 +1,34 @@
+# Class: software::tcpdump
+#
+# This is the git class in the software module.
+#   
+# Parameters:
+#   
+# Actions:
+#   
+# Requires:
+#   
+# Sample Usage:
+#   
+# [Remember: No empty lines between comments and class definition]
 class software::tcpdump {
-    case $operatingsystem {
-        Solaris: {
-            $package  = "SUNWtcpdump"
+    case $kernel {
+        SunOS: {
+            case $operatingsystem {
+                Solaris: {
+                    case $operatingsystemrelease {
+                        5.11: {
+                            $package  = "diagnostic/tcpdump"
+                        }
+                        default: {
+                            $package  = "SUNWtcpdump"
+                        }
+                    }
+                }
+            }
+        }
+        Linux: {
+            $package = "tcpdump"
         }
     }
 }
